062497c18871755cdf4d016e9df96ef87f714044
unknown <lovelydog011163@gmail.com>
2023-10-09 06:03:13 +0800

    STEP2

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 console.log("我是哈士奇")
 let startBtn = document.getElementById("start")
 let gamepic = document.getElementById("gamepic")
-let gameH1=document.querySelector(".game")
-let titlep=document.querySelector(".title")
 let videoBtn = document.getElementById("video")
 let informationBtn = document.getElementById("information")
-let connectBtn = document.getElementById("connect")
 let mediaBtn=document.getElementById("media")
+let titlepic=document.getElementById("titlepic")
+let nextstyleBtn=document.getElementById("next")
+let faintvideo=document.getElementById("faint")
+let image2=document.getElementById("image2")
 startBtn.addEventListener("click",function(){
     console.log("哈士奇又來了")
-    gamepic.src="image.png"
     videoBtn.style.display = 'none';
-    gameH1.style.display = 'none';
-    titlep.style.display = 'none';
     startBtn.style.display = 'none';
     informationBtn.style.display = 'none';
-    connectBtn.style.display = 'none';
+    titlepic.style.display = 'none';
+    mediaBtn.style.display ='none';
 })
 videoBtn.addEventListener("click",function(){
     console.log("鯊魚寶寶來了喔( ´థ౪థ）σ")
@@ -24,10 +23,6 @@ videoBtn.addEventListener("click",function(){
 informationBtn.addEventListener("click",function(){
     console.log("長官我可以加薪嗎QAQ")
     window.location.href="https://hpcod.mohw.gov.tw/HospWeb/RWD/PageType/acp/introduction.aspx"
-})
-connectBtn.addEventListener("click",function(){
-    console.log("其實我也不知道要聯絡誰TAT")
-    window.location.href="https://www.cgmh.org.tw/tw/SocialService/Service#4"
 })
 mediaBtn.addEventListener("click",function(){
     let audio = document.getElementById("backgroundAudio");
@@ -39,3 +34,17 @@ mediaBtn.addEventListener("click",function(){
         mediaBtn.innerHTML="&#128263;"
     }}
 )
+startBtn.addEventListener("click",function(){
+    console.log("這樣會出現嗎")
+    let storyHtml =`
+        <div class="storybegin"> 
+            <div class="firstvideo" id="firstvideo"></div>
+            <div class="nextstyle" id="nextstyle">&#128315;</div>
+            <video class="faint" id="faint" autoplay muted loop src="開頭影片.mp4" type="video/mp4"></video>
+        </div>
+        <img id="image2" src="image.png" style="display:none;>
+    `;
+    let storyHtmla =document.querySelector('.board')
+    storyHtmla.insertAdjacentHTML('beforeend', storyHtml);
+    });
+   
